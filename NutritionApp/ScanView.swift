9484f37dedd22cc288ead91b1e612c8b3d1ae62a
J.T. Robinson <Jaittarius@hotmail.com>
2024-11-11 17:35:01 -0500

    Update ScanView to display camera capture as a sheet

--- NutritionApp/ScanView.swift
+++ NutritionApp/ScanView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 import CodeScanner
 
 struct ScanView: View {
+    @State private var isPresentingScanner = false
+    @State private var scannedCode: String?
+    @State private var upcCodeArr: [String] = []
+    
     var body: some View {
-        CodeScannerView(codeTypes: [.ean13], showViewfinder: true, simulatedData: "123456789", shouldVibrateOnSuccess: true) { response in
-            switch response {
-            case .success(let result):
-                print("Found code: \(result.string.dropFirst(1))")
-            case .failure(let error):
-                print(error.localizedDescription)
+        VStack(spacing: 10) {
+            ForEach(upcCodeArr, id: \.self) { upcCode in
+                Text("\(upcCode)")
+            }
+            Button("Scan Code") {
+                isPresentingScanner = true
+            }.buttonStyle(.bordered)
+                .sensoryFeedback(.success, trigger: scannedCode)
+            
+            Text("Scan a UPC code to begin")
+        }
+        .sheet(isPresented: $isPresentingScanner) {
+            HStack {
+                Spacer()
+                Button("Done") {
+                    isPresentingScanner = false
+                }.padding()
+            }
+            CodeScannerView(codeTypes: [.ean13], scanMode: .oncePerCode, showViewfinder: true) { response in
+                if case let .success(result) = response {
+                    scannedCode = String(result.string.dropFirst(1))
+                    upcCodeArr.append(scannedCode!)
+                }
             }
         }
     }
